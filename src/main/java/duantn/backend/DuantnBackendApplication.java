0fdfc7e26d0ad70575d9724dc391837b6ccd183f
ThanhBinh <thanhbinhgtv@gmail.com>
2021-01-26 10:16:41 +0700

    Them Wagger API

--- src/main/java/duantn/backend/DuantnBackendApplication.java
+++ src/main/java/duantn/backend/DuantnBackendApplication.java
@@ -1,9 +1,12 @@
 package duantn.backend;
 
+import duantn.backend.authentication.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class DuantnBackendApplication {
 
     public static void main(String[] args) {
