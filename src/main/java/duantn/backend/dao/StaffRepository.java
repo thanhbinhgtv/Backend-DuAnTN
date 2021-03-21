c026e7199cc513a36a6c390078d47fab57493ef4
sieunhanbay1997@gmail.com <duc761230>
2021-03-21 09:36:41 +0700

    sửa lại thông báo = tiếng việt

--- src/main/java/duantn/backend/dao/StaffRepository.java
+++ src/main/java/duantn/backend/dao/StaffRepository.java
@@ -1,24 +1,29 @@
 package duantn.backend.dao;
 
+import duantn.backend.helper.Helper;
 import duantn.backend.model.entity.Staff;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
-    List<Staff> findByDeletedFalse(Sort sort);
-    List<Staff> findByDeletedFalse();
+    Page<Staff> findByNameLikeAndDeletedFalseOrEmailLikeAndDeletedFalseOrPhoneLikeAndDeletedFalse(
+            String name, String email, String phone, Pageable pageable
+    );
+    Page<Staff> findByNameLikeAndDeletedTrueOrEmailLikeAndDeletedTrueOrPhoneLikeAndDeletedTrue(
+            String name, String email, String phone, Pageable pageable
+    );
+    Page<Staff> findByNameLikeOrEmailLikeOrPhoneLike(
+            String name, String email, String phone, Pageable pageable
+    );
 
     Staff findByStaffIdAndDeletedFalse(Integer staffId);
-
-    List<Staff> findByNameLikeAndDeletedFalse(String name);
-    List<Staff> findByEmailLikeAndDeletedFalse(String name);
-    List<Staff> findByPhoneLikeAndDeletedFalse(String name);
 
     List<Staff> findByDeletedTrue();
 
--- src/main/java/duantn/backend/model/dto/input/StaffUpdateDTO.java
+++ src/main/java/duantn/backend/model/dto/input/StaffUpdateDTO.java
@@ -11,32 +11,32 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class StaffUpdateDTO {
-    @NotNull(message = "STAFF_ID_IS_NOT_NULL")
+    @NotNull(message = "Staff id không được trống")
     private Integer staffId;
 
-    @Email(message = "EMAIL_IS_NOT_VALID")
-    @NotBlank(message = "EMAIL_IS_NOT_NULL")
+    @Email(message = "Email không đúng định dạng")
+    @NotBlank(message = "Email không được trống")
     private String email;
 
-    @Size(min = 3, max=50, message = "NAME_MUST_BE_3_TO_50_CHARACTER")
+    @Size(min = 3, max=50, message = "Tên phải có từ 3 đến 50 kí tự")
     private String name;
 
-    @Size(min = 9, max = 12, message = "CARD_ID_MUST_BE_9_12_NUMBER")
+    @Size(min = 9, max = 12, message = "Số CMND phải có từ 9-12 kí tự")
     private String cardId;
 
-    @NotNull(message = "BIRTHDAY_IS_NOT_NULL")
+    @NotNull(message = "Ngày sinh không được trống")
     private long birthday;
 
-    @NotNull(message = "GENDER_IS_NOT_NULL")
+    @NotNull(message = "Giới tính không được trống")
     private boolean gender;
 
-    @NotNull(message = "ROLE_IS_NOT_NULL")
+    @NotNull(message = "Vai trò không được trống")
     private boolean role;
 
-    @NotBlank(message = "ADDRESS_IS_NOT_BLANK")
+    @NotBlank(message = "Địa chỉ không được trống")
     private String address;
 
-    @Size(message = "PHONE_MUST_BE_9_TO_11_NUMBER")
+    @Size(min=9,max = 11,message = "SĐT phải có 9-11 kí tự")
     private String phone;
 
     private String image;
