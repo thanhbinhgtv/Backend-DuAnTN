326f8801962911fbd557d75b6ec024cb37ce9760
sieunhanbay1997@gmail.com <duc761230>
2021-04-15 10:34:36 +0700

    add upload firebase

---
+++ src/main/java/duantn/backend/component/uploadFirebase/FirebaseFileService.java
@@ -0,0 +1,65 @@
+package duantn.backend.component.uploadFirebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created with YourComputer.
+ * Author: DUC_PRO
+ * Date: 15/04/2021
+ * Time: 10:01 SA
+ */
+
+@Service
+public class FirebaseFileService {
+
+    private Storage storage;
+
+    @EventListener
+    public void init(ApplicationReadyEvent event) {
+        try {
+            ClassPathResource serviceAccount = new ClassPathResource("firebase.json");
+            storage = StorageOptions.newBuilder().
+                    setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream())).
+                    setProjectId("healthy-ion-297905").build().getService();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String saveTest(MultipartFile file) throws IOException {
+        String imageName = generateFileName(file.getOriginalFilename());
+        Map<String, String> map = new HashMap<>();
+        map.put("firebaseStorageDownloadTokens", imageName);
+        //BUCKET=PROJECT_ID + "appspot.com"
+        BlobId blobId = BlobId.of("healthy-ion-297905.appspot.com", imageName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setMetadata(map)
+                .setContentType(file.getContentType())
+                .build();
+        storage.create(blobInfo, file.getBytes());
+        return imageName;
+    }
+
+    private String generateFileName(String originalFileName) {
+        return UUID.randomUUID().toString() + "." + getExtension(originalFileName);
+    }
+
+    private String getExtension(String originalFileName) {
+        return StringUtils.getFilenameExtension(originalFileName);
+    }
+}
---
+++ src/main/java/duantn/backend/component/uploadFirebase/UploadController.java
@@ -0,0 +1,62 @@
+package duantn.backend.component.uploadFirebase;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import duantn.backend.authentication.CustomException;
+import duantn.backend.model.dto.output.Message;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UploadController {
+    private final FirebaseFileService firebaseFileService;
+
+    public UploadController(FirebaseFileService firebaseFileService) {
+        this.firebaseFileService = firebaseFileService;
+    }
+
+    @PostMapping("/firebase/upload")
+    public Map<String, String> create(@RequestParam(name = "files") List<MultipartFile> files) {
+        if(files.size()>0){
+            Map<String, String> imageLinks=new HashMap<>();
+            int index=0;
+            for (MultipartFile file:files){
+                try {
+                    String fileName = firebaseFileService.saveTest(file);
+                    // do whatever you want with that
+                    String url="https://firebasestorage.googleapis.com/v0/b/healthy-ion-297905.appspot.com/o/"+fileName+"?alt=media&token="+fileName;
+
+                    imageLinks.put("image"+index, url);
+                    index++;
+                } catch (Exception e) {
+                    //  throw internal error;
+                    e.printStackTrace();
+                    throw new CustomException("Upload image thất bại");
+                }
+            }
+            return imageLinks;
+        }else {
+            throw new CustomException("Không có ảnh nào để upload");
+        }
+    }
+
+
+    @GetMapping("/split")
+    public String split(@RequestParam String string){
+        String[] list=string.split("___");
+        String chuoiCon="";
+        List<String> list1= Arrays.asList(list);
+        for(String s: list){
+            chuoiCon+=s+"\n";
+            System.out.println("la: "+s);
+        }
+        return chuoiCon;
+    }
+}
